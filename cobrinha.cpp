b2c249bbafdfe47fe2fd8a6c751f894f51ed79b6
Alexandre Matias <alexandrehmatias@hotmail.com>
2017-05-27 12:52:16 -0300

    novo arquivo (#2)
    não carrega bitmaps

--- cobrinha.cpp
+++ cobrinha.cpp
@@ -8,15 +8,14 @@
 #include <allegro5/allegro_audio.h>
 #include <allegro5/allegro_acodec.h>
 
-//modifiquei cobrinha
-
 using namespace std;
 
 const float FPS = 60;
 const int SCREEN_W = 700;
 const int SCREEN_H = 500;
-ALLEGRO_DISPLAY *display = NULL;
 
+ALLEGRO_BITMAP *fundo;
+ALLEGRO_BITMAP *bmp;
 bool comAudio = true;
 
 bool teclas[4] = { false, false, false, false };
@@ -33,21 +32,22 @@ protected:
 public:
 	Segmento()
 	{
+        cout << "construiu segmentos" << endl;
 		x = 200;
 		y = 300;
+        bmp = al_load_bitmap("fundo.jpg");
+	    if(!bmp)
+		{
+			printf("failed to create bitmap!\n");
+		}
 	}
 	Segmento(int _x, int _y)
 	{
 		x = _x;
-		y = _y;		
+		y = _y;
 	}
 	void imprimeSegmentos()
 	{
-		bmp = al_load_bitmap("cabeca.png");
-	    if(!bmp)
-		{
-			printf("faailed to create bitmap!\n");
-		}
 	    al_draw_bitmap(bmp, x, y, 0);
 		al_flip_display();
 	}
@@ -70,28 +70,26 @@ class Cobrinha : public Segmento{
 	int tamanho;
 	static const int tamanho_maximo = 30;
 	Segmento cabeca;
-	Segmento **segmentos;
+	//Segmento **segmentos;
 public:
 	Cobrinha()
 	{
 		tamanho = 1;
-		segmentos = new Segmento*[tamanho_maximo];
-		segmentos[0] = new Segmento(cabeca.getCoordenadaX() - 65, cabeca.getCoordenadaY());
-
+		//segmentos = new Segmento*[tamanho_maximo];
+		//segmentos[0] = new Segmento(cabeca.getCoordenadaX() - 65, cabeca.getCoordenadaY());
 	}
 	void imprimeCobrinha()
 	{
 		cabeca.imprimeSegmentos();
-        for (int i = 0; i < tamanho; i++)
-            segmentos[i]->imprimeSegmentos();
+        //for (int i = 0; i < tamanho; i++)
+            //segmentos[i]->imprimeSegmentos();
         al_flip_display();
-
     }
 	~Cobrinha()
 	{
-	    for (int i = 0; i < tamanho; i++)
-            delete segmentos[i];
-        delete[] segmentos;
+	    //for (int i = 0; i < tamanho; i++)
+            //delete segmentos[i];
+        //delete[] segmentos;
         cout << "destruiu cobrinha" << endl;
 	}
 };
@@ -120,76 +118,71 @@ public:
   	}
 };
 
-class criaDisplay
+/*class criaDisplay
 {
+    ALLEGRO_DISPLAY *display = NULL;
 public:
-	criaDisplay()
-	{
-		display = al_create_display(SCREEN_W, SCREEN_H);
-   		if(!display)
-   		{
-   		   cout << "failed to create display!\n";
-   		}
-   		cout << "chega aqui" << endl;
-	}
-
-	void set_target()
+    criaDisplay()
+    {
+        display = al_create_display(SCREEN_W, SCREEN_H);
+        if(!display)
+        {
+            cout << "failed to create display!\n";
+        }
+    }
+    void set_target()
 	{
 		al_set_target_bitmap(al_get_backbuffer(display));
 	}
 	~criaDisplay()
 	{
-		al_destroy_display(display);
-		cout << "destruiu display" << endl;
+	    al_destroy_display(display);
 	}
-};
+};*/
 
 int main()
 {
-   	srand((unsigned)time(NULL));
-	
-   	//Reponsável pela impressão na tela
-   	//Fila de eventos
-   	//ALLEGRO_EVENT_QUEUE *event_queue = NULL;
-   	//Eventos de tempo (ocorrem a cada X tempo)
-   	//ALLEGRO_TIMER *timer = NULL;
+    srand((unsigned)time(NULL));
 
-	criaDisplay display;
-	cout << "chega aqui" << endl;
-	criaFundo fundo;
+    //Reponsável pela impressão na tela
+    //criaDisplay display;
+    ALLEGRO_DISPLAY *display = NULL;
+    //Fila de eventos
+    //ALLEGRO_EVENT_QUEUE *event_queue = NULL;
+    //Eventos de tempo (ocorrem a cada X tempo)
+    //ALLEGRO_TIMER *timer = NULL;
+
+    criaFundo fundo;
 	Cobrinha cobrinha;
 
-   	//Iniciamos o Allegro
-   	if(!al_init()) {
-   	   cout << "failed to initialize allegro!\n";
-   	   return -1;
-   	}
+    //Iniciamos o Allegro
+    if(!al_init()) {
+        cout << "failed to initialize allegro!\n";
+        return -1;
+    }
 
-  	// Inicializa o add-on para utilização de imagens
+    // Inicializa o add-on para utilização de imagens
     al_init_image_addon();
+
+
+    display = al_create_display(SCREEN_W, SCREEN_H);
+    if(!display) {
+       cout << "failed to create display!\n";
+       return -1;
+    }
 
     if(fundo.imagemDeFundo() == -1)
       return -1;
 
-  	display.set_target();
-	
-   	cobrinha.imprimeCobrinha();
-	//cobrinha.imprimeCobrinha();
-	
-	al_rest(5.0);
-	
+    //display.set_target();
+    al_set_target_bitmap(al_get_backbuffer(display));
+
+    //cobrinha.imprimeCobrinha();
+    //cobrinha.imprimeCobrinha();
+
+    al_rest(5.0);
+
+    al_destroy_display(display);
+
 	return 0;
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
